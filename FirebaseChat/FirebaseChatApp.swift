96b9e09f69738a0fc1da1ccfaa499edd1c6cfb6e
IncognitoCxde <benissaeaston@gmail.com>
2025-12-05 13:01:40 +0500

    Initial Commit

---
+++ FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by iMacbook on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
